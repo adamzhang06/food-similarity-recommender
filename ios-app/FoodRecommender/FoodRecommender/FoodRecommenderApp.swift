ada4ea966de6d4ba3b2370c39aff863640554113
adamzhang06 <reachadamzhang@gmail.com>
2026-01-19 03:05:47 -0500

    Initialize iOS app project structure with essential files and configurations from Xcode

---
+++ ios-app/FoodRecommender/FoodRecommender/FoodRecommenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodRecommenderApp.swift
+//  FoodRecommender
+//
+//  Created by Adam Zhang on 1/19/26.
+//
+
+import SwiftUI
+
+@main
+struct FoodRecommenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
