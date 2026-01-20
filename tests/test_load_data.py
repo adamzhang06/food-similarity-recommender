f7f7f427e56c8447aa50feeff5424665d3c251f1
adamzhang06 <reachadamzhang@gmail.com>
2026-01-19 23:48:28 -0500

    Added initial package structure with setup.py so that I can easily import different files into others for a workable pipeline.

--- tests/test_load_data.py
+++ tests/test_load_data.py
@@ -1,3 +1,3 @@
-# from model.load_data import load_recipes
+from model.load_data import load_recipes
 
-# recipes_ar = load_recipes("data/raw/recipes_raw_nosource_ar.json")
+recipes_ar = load_recipes("data/raw/recipes_raw_nosource_ar.json")
